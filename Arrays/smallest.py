93f8b02a72911ab2e1030c478d65fc9403331a52
Aditya-Patil0801 <adi.patil0801@gmail.com>
2026-07-05 20:45:43 +0530

    Added Smallest Num in Array

--- Arrays/smallest.py
+++ Arrays/smallest.py
@@ -1,6 +1,25 @@
 #Find Smallest Num in Array
 
+
+# #METHOD-1
+# arr=list(map(int, input("Enter the numbers:").split()))
+# print(arr)
+# arr.sort()
+# print(f"Smallest Number is {arr[0]}.")
+
+
+# #METHOD-2
+# arr=list(map(int, input("Enter the numbers:").split()))
+# print (arr)
+# smallest=arr[0]
+
+# for i in range(len(arr)):
+#     if arr[i]< smallest:
+#         smallest=arr[i]
+        
+# print(f"Smallest Number is {smallest}.")
+
+#method-3
 arr=list(map(int, input("Enter the numbers:").split()))
-print(arr)
-arr.sort()
-print(f"Smallest Number is {arr[0]}.")
+print (arr)
+print(f"Smallest number is {min(arr)}.")
