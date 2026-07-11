a2db87c1660e92dabbc543ccf56a0b368f276b74
Aditya-Patil0801 <adi.patil0801@gmail.com>
2026-07-11 17:12:03 +0530

    Solved: 2.Largest num in array.

--- Arrays/largest.py
+++ Arrays/largest.py
@@ -1,24 +1,14 @@
 #Find Largest Num in Array
 
-#METHOD-1
+def Largest(arr):
+    largest=arr[0]
+    
+    for i in range(len(arr)):
+        if arr[i] > largest:
+            largest=arr[i]
+            
+    return largest
+
+
 arr=list(map(int, input("Enter the elements:").split()))
-print(arr)
-arr.sort()
-print(f"{arr[-1]} is the largest element in an array.")
-
-
-#METHOD-2
-arr1=list(map(int, input("Enter the array elements:").split()))
-print(arr1)
-largest=arr1[0]
-for i in range (len(arr1)):
-    if arr1[i]>largest:
-        largest=arr1[i]
-        
-print(f"{largest} is the largest element in an array.")
-
-
-#METHOD-3
-arr2=list(map(int ,input("Enter the elements :").split()))
-print(arr2)
-print(f"{max(arr2)} is the largest element in an array.")
+print("Largest element is ",Largest(arr))
