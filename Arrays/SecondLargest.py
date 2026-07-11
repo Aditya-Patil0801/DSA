7677e294387ff3c0d0b5594b188a81d3ffd2ecc2
Aditya-Patil0801 <adi.patil0801@gmail.com>
2026-07-11 17:29:10 +0530

    Solved: 4.Second Largest element in Array.

--- Arrays/SecondLargest.py
+++ Arrays/SecondLargest.py
@@ -1,15 +1,8 @@
-#Find 2nd Largest element 
+def Sec_largest(arr):
+    arr.sort()
+    secondlargest=arr[-2]
+    
+    return secondlargest    
 
-
-#METHOD-1
-arr=list(map(int, input("Enter the Elements:").split()))
-print(arr)
-arr.sort()
-print(f"{arr[-2]} is the second Largest element in an array.")
-
-
-#METHOD-2
-arr1=list(map(int, input("Enter the Elements:").split()))
-print(arr1)
-arr1.sort(reverse=True) #REVERSE-SORT
-print(f"{arr1[1]} is the second Largest element in an array.")
+arr = list(map(int, input("Enter the elements:").split()))
+print("Second Largest element is :", Sec_largest(arr))
