552511d68c048eef8ba1239a5fc057290b1ddcf7
Aditya-Patil0801 <adi.patil0801@gmail.com>
2026-07-11 17:07:29 +0530

    Solved: 1.Smallest.py

--- Arrays/smallest.py
+++ Arrays/smallest.py
@@ -1,25 +1,13 @@
-#Find Smallest Num in Array
+#wap to print the smallest number in array.
 
-
-#METHOD-1
-arr=list(map(int, input("Enter the elements:").split()))
-print(arr)
-arr.sort()
-print(f"Smallest element is {arr[0]}.")
-
-
-#METHOD-2
-arr1=list(map(int, input("Enter the elements:").split()))
-print (arr1)
-smallest=arr1[0]
-
-for i in range(len(arr1)):
-    if arr1[i]< smallest:
-        smallest=arr1[i]
+def Smallest(arr):
+    smallest=arr[0]
+    
+    for i in range(len(arr)):
+        if arr[i]<smallest:
+            smallest=arr[i]
         
-print(f"Smallest element is {smallest}.")
+    return smallest
 
-#method-3
-arr2=list(map(int, input("Enter the elements:").split()))
-print (arr2)
-print(f"Smallest eleement is {min(arr2)}.")
+arr=list(map(int, input("Enter the elements:").split()))
+print("smallest element is ",Smallest(arr))
