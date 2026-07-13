eb89d04cab330f08805154057d95fe3ee1d8cd46
Aditya-Patil0801 <adi.patil0801@gmail.com>
2026-07-13 22:56:44 +0530

    Solved: 9.insertion at beginnig of array.

---
+++ Arrays/insert_beginning.py
@@ -0,0 +1,11 @@
+#insert element at the end of the array
+
+
+def insert_arr(arr):
+    arr.append(x) #adding at the end
+
+    return arr
+
+arr=[1,2,3,4,5]
+x=int(input("Enter element to insert:"))
+print(insert_arr(arr))
